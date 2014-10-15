12b7d0b9fa28c53ad572f53b438d54936bbc8503
Andrew Rogers <abennobashi@gmail.com>
2014-10-15 15:48:48 -0700

    Add the monitor script
    - Monitors the websocket for ledger close events, and checks the
    transactions for the gateway account for new TrustSet transactions.
    For each new TrustSet transaction, automatically pay the trusting account
    a given number of the currency.
    - Saves the last ledger seen to a text file, and uses this as the ledger
    min for the next time we get the account transactions.

---
+++ config.js
@@ -0,0 +1,12 @@
+var config = {};
+
+config.STELLARD_IP="test.stellar.org";
+config.STELLARD_WEBSOCKET_PORT=9001;
+config.STELLARD_RPC_PORT=9002;
+
+config.GATEWAY_ADDRESS="gM4Fpv2QuHY4knJsQyYGKEHFGw3eMBwc1U";
+config.GATEWAY_SECRET="sfwtwgV3zHekZMm6F2cNPzEGzogQqPMEZcdVftKnrstngZvotYr";
+config.GATEWAY_CURRENCY="MOZ";
+config.GIVEAWAY_AMOUNT=1000;
+
+module.exports = config;
